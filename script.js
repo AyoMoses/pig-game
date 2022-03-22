dfa061750365d66a4ad71c3f48a77c5db10f1475
ayoMoses <odukoyaayodeji@gmail.com>
2022-03-22 10:41:33 +0100

    hiding dice and textContent starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// const score0 = document.getElementById('score--0');
+// const score1 = document.getElementById('score--1');
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
